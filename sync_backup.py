557fa3831cb51bcf2ad3c02cfd36f9e59854abcd
glwhappen <1597721684@qq.com>
2023-05-14 21:06:20 +0800

    添加快照功能，提高拷贝速度，但是目前用处不大

--- sync_backup.py
+++ sync_backup.py
@@ -8,7 +8,20 @@ import fnmatch
 import datetime
 import hashlib
 import yaml # pip install pyyaml
+import pickle
 
+# 函数：保存备份快照
+def save_snapshot(snapshot, snapshot_file):
+    with open(snapshot_file, 'wb') as f:
+        pickle.dump(snapshot, f)
+
+# 函数：加载备份快照
+def load_snapshot(snapshot_file):
+    if os.path.exists(snapshot_file):
+        with open(snapshot_file, 'rb') as f:
+            return pickle.load(f)
+    else:
+        return {}
 
 ## 函数：读取配置文件
 def read_config(config_file):
@@ -54,12 +67,9 @@ def should_ignore(path, ignore_patterns):
     return False
 
 # 函数：拷贝目录并忽略特定文件，同时统计新增、删除和修改的文件数量
-def copy_dir(src, dst, ignore_patterns, stats={'copied': 0, 'deleted': 0, 'modified': 0, 'checked': 0}, counter={'count': 0}):
+def copy_dir(src, dst, ignore_patterns, snapshot, stats={'copied': 0, 'deleted': 0, 'modified': 0, 'checked': 0}, counter={'count': 0}):
     names = os.listdir(src)  # 获取源文件夹中的文件和文件夹列表
     os.makedirs(dst, exist_ok=True)  # 确保目标文件夹存在
-
-
-
 
     ignored_names = set()  # 存放需要忽略的文件和文件夹名称
 
@@ -91,6 +101,16 @@ def copy_dir(src, dst, ignore_patterns, stats={'copied': 0, 'deleted': 0, 'modif
             if os.path.isdir(srcname):  # 如果是文件夹
                 copy_dir(srcname, dstname, ignore_patterns, stats)  # 递归拷贝文件夹
             else:  # 如果是文件
+
+                current_timestamp = os.path.getmtime(srcname)  # 获取文件的当前修改时间
+                # 如果文件没有被修改，则跳过
+                if name in dst_names and current_timestamp == snapshot.get(srcname, 0):
+                    print(f'Skipped: {srcname} {current_timestamp} {snapshot.get(srcname, 0)}')
+                    continue
+
+                # 更新文件的时间戳快照
+                snapshot[srcname] = current_timestamp
+
                 if name not in dst_names:  # 如果目标文件夹中不存在该文件
                     shutil.copy2(srcname, dstname)  # 拷贝文件
                     stats['copied'] += 1  # 记录拷贝的文件数量
@@ -116,8 +136,9 @@ def copy_dir(src, dst, ignore_patterns, stats={'copied': 0, 'deleted': 0, 'modif
     return stats  # 返回统计信息
 
 # 函数：备份多个目录到指定的备份文件夹
-def backup_dirs(dirs, backup_dir):
+def backup_dirs(dirs, backup_dir, snapshot_file):
     total_stats = {'copied': 0, 'deleted': 0, 'modified': 0, 'checked': 0}  # 初始化总的统计信息
+    snapshot = load_snapshot(snapshot_file)  # type:dict 加载快照
     for dir in dirs:  # 遍历所有需要备份的目录
         dir_name = os.path.basename(dir)  # 获取源目录的名称
         dir_backup_path = os.path.join(backup_dir, dir_name)  # 在备份路径上添加源目录的名称
@@ -128,14 +149,17 @@ def backup_dirs(dirs, backup_dir):
                 ignore_patterns = read_gitignore(gitignore_path)  # 读取.gitignore文件获取需要忽略的文件和文件夹
                 rel_path = os.path.relpath(root, dir)  # 计算相对路径
                 backup_path = os.path.join(dir_backup_path, rel_path)  # 根据相对路径计算备份路径
-                stats = copy_dir(root, backup_path, ignore_patterns)  # 拷贝目录并获取统计信息
+                stats = copy_dir(root, backup_path, ignore_patterns, snapshot)  # 拷贝目录并获取统计信息
                 total_stats['copied'] += stats['copied']  # 更新总的拷贝文件数量
                 total_stats['deleted'] += stats['deleted']  # 更新总的删除文件数量
                 total_stats['modified'] += stats['modified']  # 更新总的修改文件数量
                 total_stats['checked'] += stats['checked']  # 更新总的检查目录数量
 
+    save_snapshot(snapshot, snapshot_file)  # 保存快照
+
     print(f'Checked {total_stats["checked"]} files/directories. Copied {total_stats["copied"]} files. Deleted {total_stats["deleted"]} files. Modified {total_stats["modified"]} files.')  # 打印总的统计信息
 
 
 dirs, backup_dir = read_config('config.yml') # 读取配置文件
-backup_dirs(dirs, backup_dir) # 调用backup_dirs函数进行备份
+snapshot_file = 'snapshot.pickle'  # 快照文件
+backup_dirs(dirs, backup_dir, snapshot_file) # 调用backup_dirs函数进行备份
